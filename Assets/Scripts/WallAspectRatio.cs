4f31da7d14cc7c5c5191b751771ab7fbc2dbf5c2
EliasMitsolides <46579516+EliasMitsolides@users.noreply.github.com>
2021-04-28 17:41:44 -0400

    screen work
    not done yet, changed scaling so buttons don't get clipped, need to fix walls still

---
+++ Assets/Scripts/WallAspectRatio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallAspectRatio : MonoBehaviour
+{
+    float height;
+
+    float width; // basically height screen aspect ratio
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = Camera.main.orthographicSize * 2;
+        width = height * (Screen.width / Screen.height); // basically height screen aspect ratio
+        transform.localScale = new Vector3(width, height, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
